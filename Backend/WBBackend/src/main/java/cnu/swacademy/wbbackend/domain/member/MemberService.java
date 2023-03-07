f8127be9c544e4f77154345e99a434d527159fdd
OneEpitome <rkddlfah0319@gmail.com>
2023-03-07 21:06:53 +0900

    Enable Spring Security
    스프링 시큐리티 활성화
    스프링 시큐리티를 이용하기 위한 MemberService 생성

---
+++ Backend/WBBackend/src/main/java/cnu/swacademy/wbbackend/domain/member/MemberService.java
@@ -0,0 +1,25 @@
+package cnu.swacademy.wbbackend.domain.member;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.security.core.authority.AuthorityUtils;
+import org.springframework.security.core.userdetails.User;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.stereotype.Service;
+
+import java.util.Optional;
+
+@Service
+@RequiredArgsConstructor
+public class MemberService implements UserDetailsService {
+    private final MemberRepository memberRepository;
+
+    @Override
+    public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
+        Member member = memberRepository.findByUsername(username)
+                .orElseThrow(() -> new UsernameNotFoundException(username + "doesn't exist in MemberRepository"));
+        User user = new User(username, member.getPassword(), AuthorityUtils.createAuthorityList(member.getAuthority()));
+        return user;
+    }
+}
--- Backend/WBBackend/src/main/java/cnu/swacademy/wbbackend/security/SecurityConfiguration.java
+++ Backend/WBBackend/src/main/java/cnu/swacademy/wbbackend/security/SecurityConfiguration.java
@@ -1,17 +1,35 @@
 package cnu.swacademy.wbbackend.security;
 
+import cnu.swacademy.wbbackend.domain.member.MemberService;
+import lombok.RequiredArgsConstructor;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.web.SecurityFilterChain;
 
 @Configuration
+@RequiredArgsConstructor
+@EnableWebSecurity
 public class SecurityConfiguration {
+
+    private final MemberService memberService;
+
     @Bean
     public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
         http
-                .authorizeHttpRequests((authz) -> authz
-                        .anyRequest().permitAll()
+                .userDetailsService(memberService)
+                .formLogin()
+                .loginPage("/api/login")
+                .usernameParameter("username")
+                .passwordParameter("password")
+                .and()
+                .logout()
+                .logoutUrl("/api/logout")
+                .and()
+                .csrf().disable()
+                .authorizeHttpRequests((auth) ->
+                        auth.requestMatchers("/api/authenticated").authenticated()
                 );
         return http.build();
     }
