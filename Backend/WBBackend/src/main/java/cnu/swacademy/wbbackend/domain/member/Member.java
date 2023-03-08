dc6176176d4e409206e502f3bac310a941372a9a
OneEpitome <rkddlfah0319@gmail.com>
2023-03-08 22:40:42 +0900

    Modify Member Entity
    멤버 객체를 JPA 에 사용할 수 있도록 Entity 로 수정

--- Backend/WBBackend/src/main/java/cnu/swacademy/wbbackend/domain/member/Member.java
+++ Backend/WBBackend/src/main/java/cnu/swacademy/wbbackend/domain/member/Member.java
@@ -1,15 +1,28 @@
 package cnu.swacademy.wbbackend.domain.member;
 
+import jakarta.persistence.*;
 import lombok.*;
 
 @Getter
 @Setter
 @NoArgsConstructor
+@Entity(name = "member")
 public class Member {
-    private Long Id;
+    @Id
+    @Column(name = "id", nullable = false, unique = true)
+    @GeneratedValue(strategy = GenerationType.SEQUENCE)
+    private Long id;
+
+    @Column(name = "username", nullable = false, unique = true)
     private String username;
+
+    @Column(name = "password", nullable = false, unique = true)
     private String password;
+
+    @Column(name = "nickname", nullable = false, unique = true)
     private String nickname;
+
+    @Column(name = "authority", nullable = false, unique = true)
     private String authority;
 
     public Member(String username, String password, String nickname, String authority) {
