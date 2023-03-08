6c51ff9e104e34713bc45950023a605952191927
OneEpitome <rkddlfah0319@gmail.com>
2023-03-08 22:41:38 +0900

    Convert MemberRepository to JPA
    Map 으로 구현했던 MemberRepository 를 JPARepository 로 변환합니다.

--- Backend/WBBackend/src/main/java/cnu/swacademy/wbbackend/domain/member/MemberRepository.java
+++ Backend/WBBackend/src/main/java/cnu/swacademy/wbbackend/domain/member/MemberRepository.java
@@ -2,6 +2,7 @@ package cnu.swacademy.wbbackend.domain.member;
 
 import jakarta.annotation.PostConstruct;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.stereotype.Repository;
 
@@ -9,25 +10,7 @@ import java.util.HashMap;
 import java.util.Map;
 import java.util.Optional;
 
-@Slf4j
 @Repository
-public class MemberRepository {
-    private static Map<Long, Member> repository = new HashMap<>();
-    private static Long sequence = 0L;
-    public Member save(Member member) {
-        member.setPassword(new BCryptPasswordEncoder().encode(member.getPassword()));
-        repository.put(++sequence, member);
-        log.info("Member Created, username : {}", member.getUsername());
-        return member;
-    }
-
-    public Optional<Member> findByUsername(String username) {
-        return repository.values()
-                .stream().filter(m -> m.getUsername().equals(username))
-                .findFirst();
-    }
-
-    public void clear() {
-        repository.clear();
-    }
+public interface MemberRepository extends JpaRepository<Member, Long> {
+    Optional<Member> findMemberByUsername(String username);
 }
