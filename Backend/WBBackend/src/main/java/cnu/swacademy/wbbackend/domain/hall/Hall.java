2654f28e87cc5f80e9211b09b8f0236bae91a5c1
Minju Song <tisu79@naver.com>
2023-03-10 00:35:32 +0900

    Getter, Setter 어노테이션 추가 후 Review, Seat 양방향 설정.

--- Backend/WBBackend/src/main/java/cnu/swacademy/wbbackend/domain/hall/Hall.java
+++ Backend/WBBackend/src/main/java/cnu/swacademy/wbbackend/domain/hall/Hall.java
@@ -2,11 +2,15 @@ package cnu.swacademy.wbbackend.domain.hall;
 
 import cnu.swacademy.wbbackend.domain.seat.Seat;
 import jakarta.persistence.*;
+import lombok.Getter;
+import lombok.Setter;
 
 import java.util.ArrayList;
 import java.util.List;
 
 @Entity
+@Getter
+@Setter
 @Table(name="hall")
 public class Hall {
 
