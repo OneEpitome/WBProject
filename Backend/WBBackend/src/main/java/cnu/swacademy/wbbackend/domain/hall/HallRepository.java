e4700be16de43e06288f0efcaf89cb5c93d72504
OneEpitome <rkddlfah0319@gmail.com>
2023-03-13 00:49:24 +0900

    Update HallRepository
    HallRepository 에 hallName 으로 Hall Entity 를 찾는 메소드를 추가하였습니다.

--- Backend/WBBackend/src/main/java/cnu/swacademy/wbbackend/domain/hall/HallRepository.java
+++ Backend/WBBackend/src/main/java/cnu/swacademy/wbbackend/domain/hall/HallRepository.java
@@ -2,5 +2,8 @@ package cnu.swacademy.wbbackend.domain.hall;
 
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.Optional;
+
 public interface HallRepository extends JpaRepository<Hall, Long> {
+    Optional<Hall> findHallByHallName(String hallName);
 }
