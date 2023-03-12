77e16085b59aea8ed4914e79671f8f23a415a84b
OneEpitome <rkddlfah0319@gmail.com>
2023-03-13 08:35:20 +0900

    Update Seat Entity
    Hall <-> Seat 간 양방향 매핑을 위한 편의 메소드 setHall 을 추가하였습니다.
    
    Hall 이 지정되지 않은 Seat 레코드는 저장될 수 없도록 nullable 속성을 추가하였습니다.

--- Backend/WBBackend/src/main/java/cnu/swacademy/wbbackend/domain/seat/Seat.java
+++ Backend/WBBackend/src/main/java/cnu/swacademy/wbbackend/domain/seat/Seat.java
@@ -1,19 +1,19 @@
 package cnu.swacademy.wbbackend.domain.seat;
 
 import cnu.swacademy.wbbackend.domain.hall.Hall;
+import cnu.swacademy.wbbackend.domain.hall.HallRepository;
+import cnu.swacademy.wbbackend.domain.hall.HallService;
 import cnu.swacademy.wbbackend.domain.review.Review;
 import jakarta.persistence.*;
-import lombok.EqualsAndHashCode;
-import lombok.Getter;
-import lombok.Setter;
+import lombok.*;
 
 import java.util.ArrayList;
 import java.util.List;
 @Getter @Setter @EqualsAndHashCode
+@NoArgsConstructor
 @Entity
 @Table(name = "seat")
 public class Seat {
-
     @Id @GeneratedValue(strategy=GenerationType.SEQUENCE)
     @Column(name = "id")
     private Long id;
@@ -22,6 +22,12 @@ public class Seat {
     private List<Review> reviewList = new ArrayList<>();
 
     @ManyToOne
-    @JoinColumn(name = "hall_id")
+    @JoinColumn(name = "hall_id", nullable = false) // Hall 이 지정되어 있지 않은 Seat 레코드는 존재 불가
     private Hall hall;
+
+    // 양방향 매핑을 위한 setHall 편의 메소드.
+    public void setHall(Hall hall) {
+        this.hall = hall;
+        hall.getSeatList().add(this);
+    }
 }
