5a80875ccb63852928db2dc30b9b1ac041e6a1b7
OneEpitome <rkddlfah0319@gmail.com>
2023-03-13 00:49:59 +0900

    Update HallService
    HallService 에 Hall Entity 를 저장하고, hallName 으로 조회하는 기능을 추가하였습니다.

--- Backend/WBBackend/src/main/java/cnu/swacademy/wbbackend/domain/hall/HallService.java
+++ Backend/WBBackend/src/main/java/cnu/swacademy/wbbackend/domain/hall/HallService.java
@@ -1,4 +1,19 @@
 package cnu.swacademy.wbbackend.domain.hall;
 
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+
+@RequiredArgsConstructor
+@Service
 public class HallService {
+    private final HallRepository hallRepository;
+
+    public Hall save(Hall hall) {
+        return hallRepository.save(hall);
+    }
+
+    public Hall findByName(String hallName) {
+        return hallRepository.findHallByHallName(hallName)
+                .orElseThrow(() -> new RuntimeException(hallName + "is Not Found"));
+    }
 }
