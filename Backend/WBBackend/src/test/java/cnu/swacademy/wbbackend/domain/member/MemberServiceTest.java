bdc26a4b755f33e613e0425fc644f7ead48edb0d
OneEpitome <rkddlfah0319@gmail.com>
2023-03-13 00:47:44 +0900

    Add MemberService Test
    MemberService 의 저장, id 를 통한 조회 기능의 테스트 코드를 추가하였습니다.

---
+++ Backend/WBBackend/src/test/java/cnu/swacademy/wbbackend/domain/member/MemberServiceTest.java
@@ -0,0 +1,75 @@
+package cnu.swacademy.wbbackend.domain.member;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+
+import java.util.Optional;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+@SpringBootTest
+public class MemberServiceTest {
+
+    @Autowired
+    MemberService memberService;
+
+    @Autowired
+    MemberRepository memberRepository;
+
+    @BeforeEach
+    void setup() {
+        memberRepository.deleteAll();
+    }
+
+    @Test
+    @DisplayName("MemberService 를 통하여 멤버를 저장할 수 있다.")
+    void save() {
+        //given
+        String username = "mockUser1";
+        String password = "password";
+        String nickname = "nick1";
+        String authority = "ROLE_USER";
+        Member member = new Member(username, password, nickname, authority);
+
+        //when
+        Member save = memberService.save(member);
+
+        //then
+        assertThat(save.getUsername()).isEqualTo(username);
+        assertThat(new BCryptPasswordEncoder().matches(password, save.getPassword())).isEqualTo(true);
+        assertThat(save.getNickname()).isEqualTo(nickname);
+        assertThat(save.getAuthority()).isEqualTo(authority);
+    }
+
+    @Test
+    @DisplayName("MemberService 를 통하여 memberId 를 통해 멤버를 조회할 수 있다.")
+    void findById() {
+        //given
+        String username = "mockUser1";
+        String password = "password";
+        String nickname = "nick1";
+        String authority = "ROLE_USER";
+        Member member = new Member(username, password, nickname, authority);
+        memberService.save(member);
+
+        //when
+        Optional<Member> findById = memberService.findById(2L);
+        /* securityConfig 의 mock 객체에 의해 2L 로 insert 됨.
+            BeforeEach 의 setup 메소드에서 제거해도 2L 로 insert.
+         */
+
+        //then
+        assertThat(findById).isNotEmpty();
+        Member save = findById.get();
+
+        assertThat(save.getUsername()).isEqualTo(username);
+        assertThat(new BCryptPasswordEncoder().matches(password, save.getPassword())).isEqualTo(true);
+        assertThat(save.getNickname()).isEqualTo(nickname);
+        assertThat(save.getAuthority()).isEqualTo(authority);
+
+    }
+}
