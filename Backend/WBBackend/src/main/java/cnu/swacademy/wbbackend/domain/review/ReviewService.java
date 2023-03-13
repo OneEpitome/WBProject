1be07418a804e75d448ddc7513d61e45185e739b
OneEpitome <rkddlfah0319@gmail.com>
2023-03-13 18:00:10 +0900

    Update ReviewService
    ReviewService 에 save, findById, deleteById 메소드를 추가하였습니다.

--- Backend/WBBackend/src/main/java/cnu/swacademy/wbbackend/domain/review/ReviewService.java
+++ Backend/WBBackend/src/main/java/cnu/swacademy/wbbackend/domain/review/ReviewService.java
@@ -1,6 +1,26 @@
 package cnu.swacademy.wbbackend.domain.review;
 
-import org.springframework.data.jpa.repository.JpaRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
 
+@RequiredArgsConstructor
+@Service
 public class ReviewService {
+
+    private final ReviewRepository reviewRepository;
+
+    public Review save(Review review) {
+        return reviewRepository.save(review);
+    }
+
+    public Review findById(Long reviewId) {
+        return reviewRepository.findById(reviewId)
+                .orElseThrow(() -> new RuntimeException("Review ID : " + reviewId + " is Not Found"));
+    }
+
+    // public Review update(Review review);
+
+    public void deleteById(Long reviewId) {
+        reviewRepository.deleteById(reviewId);
+    }
 }
