df9582ec9a2f2038c5fb903923a1b6d4cf4caedc
OneEpitome <rkddlfah0319@gmail.com>
2023-03-18 02:02:00 +0900

    Create Heart function Test
    좋아요 기능 테스트 코드

---
+++ Backend/WBBackend/src/test/java/cnu/swacademy/wbbackend/controller/HeartControllerTest.java
@@ -0,0 +1,103 @@
+package cnu.swacademy.wbbackend.controller;
+
+import cnu.swacademy.wbbackend.domain.heart.HeartRepository;
+import cnu.swacademy.wbbackend.domain.heart.HeartService;
+import cnu.swacademy.wbbackend.domain.member.Member;
+import cnu.swacademy.wbbackend.domain.member.MemberRepository;
+import cnu.swacademy.wbbackend.domain.member.MemberService;
+import cnu.swacademy.wbbackend.domain.review.Review;
+import cnu.swacademy.wbbackend.domain.review.ReviewRepository;
+import cnu.swacademy.wbbackend.domain.review.ReviewService;
+import org.assertj.core.api.Assertions;
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
+import org.springframework.test.web.servlet.result.MockMvcResultMatchers;
+import org.springframework.util.LinkedMultiValueMap;
+import org.springframework.util.MultiValueMap;
+
+import java.time.LocalDateTime;
+
+@SpringBootTest
+@AutoConfigureMockMvc
+public class HeartControllerTest {
+
+    @Autowired
+    HeartService heartService;
+    @Autowired
+    HeartRepository heartRepository;
+    @Autowired
+    MemberService memberService;
+    @Autowired
+    MemberRepository memberRepository;
+    @Autowired
+    ReviewRepository reviewRepository;
+    @Autowired
+    ReviewService reviewService;
+    @Autowired
+    MockMvc mockMvc;
+
+    @AfterEach
+    void cleanup() {
+        heartRepository.deleteAll();
+        memberRepository.deleteAll();
+        reviewRepository.deleteAll();
+    }
+
+    @Test
+    void checkHeart() throws Exception {
+        //given
+        Member member = memberRepository.save(new Member("test", "pass", "testnick", "ROLE_USER"));
+        Review review = reviewRepository.save(new Review(LocalDateTime.now(), "TITLE", "CONTENT"));
+        heartService.heartOrDelete(member, review);
+
+        //when
+        MultiValueMap<String, String> params = new LinkedMultiValueMap<>();
+        params.add("memberId", member.getId().toString());
+        params.add("reviewId", review.getId().toString());
+
+        //then
+        mockMvc.perform(MockMvcRequestBuilders.get("/api/heart").params(params))
+                .andExpect(MockMvcResultMatchers.jsonPath("status").value(true));
+    }
+
+    @Test
+    void insertHeart() throws Exception {
+        //given
+        Member member = memberRepository.save(new Member("test", "pass", "testnick", "ROLE_USER"));
+        Review review = reviewRepository.save(new Review(LocalDateTime.now(), "TITLE", "CONTENT"));
+
+        //when
+        //then
+        MultiValueMap<String, String> params = new LinkedMultiValueMap<>();
+        params.add("memberId", member.getId().toString());
+        params.add("reviewId", review.getId().toString());
+
+        mockMvc.perform(MockMvcRequestBuilders.post("/api/heart").params(params))
+                .andExpect(MockMvcResultMatchers.jsonPath("status").value(true));
+        Assertions.assertThat(reviewRepository.findById(review.getId()).get().getHeart_count()).isEqualTo(1L);
+    }
+
+    @Test
+    void deleteHeart() throws Exception {
+        //given
+        Member member = memberRepository.save(new Member("test", "pass", "testnick", "ROLE_USER"));
+        Review review = reviewRepository.save(new Review(LocalDateTime.now(), "TITLE", "CONTENT"));
+        heartService.heartOrDelete(member, review);
+
+        //when
+        //then
+        MultiValueMap<String, String> params = new LinkedMultiValueMap<>();
+        params.add("memberId", member.getId().toString());
+        params.add("reviewId", review.getId().toString());
+
+        mockMvc.perform(MockMvcRequestBuilders.post("/api/heart").params(params))
+                .andExpect(MockMvcResultMatchers.jsonPath("status").value(false));
+        Assertions.assertThat(reviewRepository.findById(review.getId()).get().getHeart_count()).isEqualTo(0L);
+    }
+
+}
---
+++ Backend/WBBackend/src/test/java/cnu/swacademy/wbbackend/domain/heart/HeartServiceTest.java
@@ -0,0 +1,76 @@
+package cnu.swacademy.wbbackend.domain.heart;
+
+import cnu.swacademy.wbbackend.domain.member.Member;
+import cnu.swacademy.wbbackend.domain.member.MemberRepository;
+import cnu.swacademy.wbbackend.domain.review.Review;
+import cnu.swacademy.wbbackend.domain.review.ReviewRepository;
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+
+import java.time.LocalDateTime;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+@SpringBootTest
+public class HeartServiceTest {
+
+    @Autowired
+    HeartService heartService;
+    @Autowired
+    HeartRepository heartRepository;
+
+    @Autowired
+    MemberRepository memberRepository;
+
+    @Autowired
+    ReviewRepository reviewRepository;
+
+    @AfterEach
+    void cleanup() {
+        heartRepository.deleteAll();
+        memberRepository.deleteAll();
+        reviewRepository.deleteAll();
+    }
+
+    @Test
+    void heart() {
+        //given
+        Member member = memberRepository.save(new Member("test", "pass", "testnick", "ROLE_USER"));
+        Review review = reviewRepository.save(new Review(LocalDateTime.now(), "TITLE", "CONTENT"));
+
+        //when
+        heartService.heartOrDelete(member, review);
+
+        //then
+        assertThat(reviewRepository.findById(review.getId()).get().getHeart_count()).isEqualTo(1L);
+    }
+
+    @Test
+    void delete() {
+        //given
+        Member member = memberRepository.save(new Member("test", "pass", "testnick", "ROLE_USER"));
+        Review review = reviewRepository.save(new Review(LocalDateTime.now(), "TITLE", "CONTENT"));
+        heartService.heartOrDelete(member, review);
+
+        //when
+        heartService.heartOrDelete(member, review);
+
+        //then
+        assertThat(reviewRepository.findById(review.getId()).get().getHeart_count()).isEqualTo(0L);
+    }
+
+    @Test
+    void readHeart() {
+        //given
+        Member member = memberRepository.save(new Member("test", "pass", "testnick", "ROLE_USER"));
+        Review review = reviewRepository.save(new Review(LocalDateTime.now(), "TITLE", "CONTENT"));
+        heartService.heartOrDelete(member, review);
+
+        //when
+        //then
+        assertThat(heartService.readHeart(member, review)).isEqualTo(true);
+    }
+
+}
