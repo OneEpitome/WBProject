92e2b5da527a135beb2269d9ad01b279cc19d555
Minju Song <tisu79@naver.com>
2023-03-25 19:13:11 +0900

    커밋용

--- Backend/WBBackend/src/main/java/cnu/swacademy/wbbackend/controller/ReviewController.java
+++ Backend/WBBackend/src/main/java/cnu/swacademy/wbbackend/controller/ReviewController.java
@@ -3,13 +3,17 @@ package cnu.swacademy.wbbackend.controller;
 import cnu.swacademy.wbbackend.domain.member.Member;
 import cnu.swacademy.wbbackend.domain.member.MemberService;
 import cnu.swacademy.wbbackend.domain.review.Review;
+import cnu.swacademy.wbbackend.domain.review.ReviewRepository;
 import cnu.swacademy.wbbackend.domain.review.ReviewService;
 import cnu.swacademy.wbbackend.domain.seat.Seat;
 import cnu.swacademy.wbbackend.domain.seat.SeatRepository;
-import cnu.swacademy.wbbackend.domain.seat.SeatService;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.web.bind.annotation.*;
+import org.springframework.web.multipart.MultipartFile;
+
+import java.io.File;
+import java.util.UUID;
 
 @Slf4j
 @RequiredArgsConstructor
@@ -18,15 +22,27 @@ import org.springframework.web.bind.annotation.*;
 public class ReviewController {
 
     private final SeatRepository seatRepository; // seatService 로 이동 필요
+    private final ReviewRepository reviewRepository;
     private final MemberService memberService;
     private final ReviewService reviewService;
 
     @PostMapping("/save")
-    public Review save(@ModelAttribute Review review, @RequestParam Long seatId, @RequestParam Long memberId) {
+    public Review save(@ModelAttribute Review review, @RequestParam Long seatId, @RequestParam Long memberId, MultipartFile file) throws Exception {
         Seat seat = seatRepository.findById(seatId).get();
         Member member = memberService.findById(memberId).get(); // memberService 의 findById 메소드 수정 필요
+        String projectPath = System.getProperty("user.dir") + File.separator + "src" + File.separator + "main" + File.separator + "resources" + File.separator + "static" + File.separator + "files"; // 저장 경로 지정
+
         review.setSeat(seat);
         review.setWriter(member);
+
+        UUID uuid = UUID.randomUUID(); // 식별자 생성
+        String fileName = uuid+"_"+file.getOriginalFilename();
+        File saveFile = new File(projectPath,fileName); // 빈 껍데기 생성 (경로, 이름)
+        file.transferTo(saveFile);
+
+        //경로랑 이름넣기
+        review.setFilename(fileName);
+        review.setFilepath("/files/"+fileName);
 
         return reviewService.save(review);
     }
